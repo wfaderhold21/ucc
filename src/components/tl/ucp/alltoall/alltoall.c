fa3239404966342e1d956fa7024cfd92adb9126f
ferrol aderholdt <faderholdt@nvidia.com>
2021-10-14 10:30:21 -0500

    TL/UCP: One-sided A2A

--- src/components/tl/ucp/alltoall/alltoall.c
+++ src/components/tl/ucp/alltoall/alltoall.c
@@ -11,6 +11,21 @@
 ucc_status_t ucc_tl_ucp_alltoall_pairwise_start(ucc_coll_task_t *task);
 ucc_status_t ucc_tl_ucp_alltoall_pairwise_progress(ucc_coll_task_t *task);
 
+ucc_status_t ucc_tl_ucp_alltoall_onesided_start(ucc_coll_task_t *task);
+ucc_status_t ucc_tl_ucp_alltoall_onesided_progress(ucc_coll_task_t *task);
+
+ucc_base_coll_alg_info_t
+    ucc_tl_ucp_alltoall_algs[UCC_TL_UCP_ALLTOALL_ALG_LAST + 1] = {
+        [UCC_TL_UCP_ALLTOALL_ALG_PAIRWISE] =
+            {.id   = UCC_TL_UCP_ALLTOALL_ALG_PAIRWISE,
+             .name = "pairwise",
+             .desc = "pairwise two-sided implementation"},
+        [UCC_TL_UCP_ALLTOALL_ALG_ONESIDED] =
+            {.id   = UCC_TL_UCP_ALLTOALL_ALG_ONESIDED,
+             .name = "onesided",
+             .desc = "naive, linear one-sided implementation"},
+        [UCC_TL_UCP_ALLTOALL_ALG_LAST] = {.id = 0, .name = NULL, .desc = NULL}};
+
 ucc_status_t ucc_tl_ucp_alltoall_init(ucc_tl_ucp_task_t *task)
 {
     ucc_status_t status;
@@ -36,3 +51,36 @@ ucc_status_t ucc_tl_ucp_alltoall_pairwise_init(ucc_base_coll_args_t *coll_args,
 out:
     return status;
 }
+
+ucc_status_t ucc_tl_ucp_alltoall_onesided_init(ucc_base_coll_args_t *coll_args,
+                                               ucc_base_team_t *     team,
+                                               ucc_coll_task_t **    task_h)
+{
+    ucc_tl_ucp_task_t *task;
+    task    = ucc_tl_ucp_init_task(coll_args, team);
+    *task_h = &task->super;
+
+    if (TASK_ARGS(task).mask & UCC_COLL_ARGS_FIELD_FLAGS)
+    {
+        if (TASK_ARGS(task).flags & UCC_COLL_ARGS_FLAG_IN_PLACE) {
+            tl_debug(UCC_TL_TEAM_LIB(TASK_TEAM(task)),
+                     "inplace alltoall is not supported");
+            return UCC_ERR_NOT_SUPPORTED;
+        }
+        if (!(TASK_ARGS(task).flags & UCC_COLL_ARGS_FLAG_MEM_MAPPED_BUFFERS)) {
+            tl_debug(UCC_TL_TEAM_LIB(TASK_TEAM(task)),
+                     "non memory mapped buffers are not supported");
+            return UCC_ERR_NOT_SUPPORTED;
+        }
+    }
+    if ((TASK_ARGS(task).src.info.datatype == UCC_DT_USERDEFINED) ||
+        (TASK_ARGS(task).dst.info.datatype == UCC_DT_USERDEFINED)) {
+        tl_debug(UCC_TL_TEAM_LIB(TASK_TEAM(task)),
+                 "user defined datatype is not supported");
+        return UCC_ERR_NOT_SUPPORTED;
+    }
+    task->super.post     = ucc_tl_ucp_alltoall_onesided_start;
+    task->super.progress = ucc_tl_ucp_alltoall_onesided_progress;
+    return UCC_OK;
+}
+
---
+++ src/components/tl/ucp/alltoall/alltoall_onesided.c
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) Mellanox Technologies Ltd. 2021.  ALL RIGHTS RESERVED.
+ *
+ * See file LICENSE for terms.
+ */
+
+#include "config.h"
+#include "tl_ucp.h"
+#include "alltoall.h"
+#include "core/ucc_progress_queue.h"
+#include "utils/ucc_math.h"
+#include "tl_ucp_sendrecv.h"
+
+ucc_status_t ucc_tl_ucp_alltoall_onesided_start(ucc_coll_task_t *ctask)
+{
+    ucc_tl_ucp_task_t *task = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
+    ucc_tl_ucp_team_t *team = TASK_TEAM(task);
+    ucc_status_t       status;
+
+    task->barrier.phase = 0;
+
+    task->super.super.status = UCC_INPROGRESS;
+    status = ucc_tl_ucp_alltoall_onesided_progress(&task->super);
+    if (UCC_INPROGRESS == status) {
+        ucc_progress_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
+        return UCC_OK;
+    }
+    task->super.super.status = status;
+    ucc_task_complete(ctask);
+
+    return UCC_OK;
+}
+
+ucc_status_t ucc_tl_ucp_alltoall_onesided_progress(ucc_coll_task_t *ctask)
+{
+    ucc_tl_ucp_task_t *task   = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
+    ucc_tl_ucp_team_t *team   = TASK_TEAM(task);
+    ptrdiff_t          src    = (ptrdiff_t)TASK_ARGS(task).src.info.buffer;
+    ptrdiff_t          dest   = (ptrdiff_t)TASK_ARGS(task).dst.info.buffer;
+    size_t             nelems = TASK_ARGS(task).src.info.count;
+    ucc_rank_t         mype   = team->rank;
+    ucc_rank_t         npes   = team->size;
+    int *              phase  = &task->barrier.phase;
+    ucc_status_t       status = UCC_INPROGRESS;
+    long *             pSync  = TASK_ARGS(task).pSync;
+    ucc_rank_t         peer;
+
+    if (*phase == 1) {
+        goto wait;
+    }
+
+    dest             = dest + mype * nelems;
+    ucc_rank_t start = (mype + 1) % npes; 
+    ucc_tl_ucp_put_nb((void *)(src + start * nelems), (void *)dest, nelems,
+                      start, team, task);
+    ucc_tl_ucp_atomic_inc(pSync, start, team, task);
+    ucc_tl_ucp_ep_flush(start, team, task);
+    
+    for (peer = (start + 1) % npes; peer != start; peer = (peer + 1) % npes) {
+        ucc_tl_ucp_put_nb((void *)(src + peer * nelems), (void *)dest, nelems,
+                          peer, team, task);
+        ucc_tl_ucp_atomic_inc(pSync, peer, team, task);
+        ucc_tl_ucp_ep_flush(peer, team, task);
+    }
+    *phase = 1;
+
+wait:
+    if (*pSync < npes - 1) {
+        return UCC_INPROGRESS;
+    }
+
+    *pSync = -1;
+
+    task->super.super.status = UCC_OK;
+    ucc_task_complete(ctask);
+    return UCC_OK;
+}
+
