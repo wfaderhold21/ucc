0bd190cd98bd3c930d971bf8b2b03d3fe710feaa
ferrol aderholdt <faderholdt@nvidia.com>
2021-10-18 08:06:42 -0700

    snapshot of current state

--- src/components/tl/ucp/alltoall/alltoall_onesided.c
+++ src/components/tl/ucp/alltoall/alltoall_onesided.c
@@ -43,29 +43,36 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_progress(ucc_coll_task_t *ctask)
     size_t nelems = TASK_ARGS(task).src.info.count;
     ucc_rank_t mype = team->rank;
     ucc_rank_t npes = team->size;
-    int * phase = &task->barrier.phase;
-    long * pSync = TASK_ARGS(task).pSync;
+//    int * phase = &task->barrier.phase;
+//    long * pSync = TASK_ARGS(task).pSync;
     ucc_rank_t peer;
-
+/*
     if (*phase == 1) {
         goto wait;
     }
-
+*/
     dest = dest + mype * nelems;
     ucc_rank_t start = (mype + 1) % npes; 
     ucc_tl_ucp_put_nb((void *)(src + start * nelems), (void *)dest, nelems, start, team, task);
-    ucc_tl_ucp_atomic_inc(pSync, start, team, task);
+//    ucc_tl_ucp_atomic_inc(pSync, start, team, task);
+    ucc_tl_ucp_ep_flush(start, team, task);
     for (peer = (start + 1) % npes; peer != start; peer = (peer + 1) % npes) {
         ucc_tl_ucp_put_nb((void *)(src + peer * nelems), (void *)dest, nelems, peer, team, task);
-        ucc_tl_ucp_atomic_inc(pSync, peer, team, task);
+//        ucc_tl_ucp_atomic_inc(pSync, peer, team, task);
+        ucc_tl_ucp_ep_flush(peer, team, task);
     }
-
-wait:
-    if (*pSync < npes - 2) {
+/*
+    for (int i = 0; i < team->size; i++) {
+        ucc_tl_ucp_ep_flush(i, team, task);
+    }*/
+//    ucc_tl_ucp_flush(team);
+//    *phase = 1;
+//wait:
+/*    if (*pSync < npes - 1) {
         return UCC_INPROGRESS;
-    }
+    }*/
       
-    *pSync = -1;
+//    *pSync = -1;
     task->super.super.status = UCC_OK;
     ucc_task_complete(ctask);
     return UCC_OK;
