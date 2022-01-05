4f8ffe326c481f38f19c42474a4875a74922da77
Ferrol Aderholdt <aderholdtwf1@ornl.gov>
2022-01-05 16:57:21 -0500

    align onesided codes

--- src/components/tl/ucp/alltoall/alltoall_onesided.c
+++ src/components/tl/ucp/alltoall/alltoall_onesided.c
@@ -17,20 +17,37 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_get_progress(ucc_coll_task_t *ctask);
 
 ucc_status_t ucc_tl_ucp_alltoall_onesided_start(ucc_coll_task_t *ctask)
 {
-    ucc_tl_ucp_task_t *task = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
-    ucc_tl_ucp_team_t *team = TASK_TEAM(task);
-    ucc_status_t       status;
+    ucc_tl_ucp_task_t *task   = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
+    ucc_tl_ucp_team_t *team   = TASK_TEAM(task);
+    ptrdiff_t          src    = (ptrdiff_t)TASK_ARGS(task).src.info.buffer;
+    ptrdiff_t          dest   = (ptrdiff_t)TASK_ARGS(task).dst.info.buffer;
+    size_t             nelems = TASK_ARGS(task).src.info.count;
+    ucc_rank_t         grank  = UCC_TL_TEAM_RANK(team);
+    ucc_rank_t         gsize  = UCC_TL_TEAM_SIZE(team);
+    ucc_rank_t         start  = (grank + 1) % gsize;
+    long *             pSync  = TASK_ARGS(task).global_work_buffer;
+    ucc_rank_t         peer;
 
-    task->super.super.status = UCC_INPROGRESS;
-    status = ucc_tl_ucp_alltoall_onesided_progress(&task->super);
-    if (UCC_INPROGRESS == status) {
-        ucc_progress_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
-        return UCC_OK;
+    ucc_tl_ucp_task_reset(task, UCC_INPROGRESS);
+    /* TODO: change when support for library-based work buffers is complete */
+    nelems = (nelems / gsize) * ucc_dt_size(TASK_ARGS(task).src.info.datatype);
+    dest   = dest + grank * nelems;
+    UCPCHECK_GOTO(ucc_tl_ucp_put_nb((void *)(src + start * nelems),
+                                    (void *)dest, nelems, start, team, task),
+                  task, out);
+    UCPCHECK_GOTO(ucc_tl_ucp_atomic_inc(pSync, start, team), task, out);
+
+    for (peer = (start + 1) % gsize; peer != start; peer = (peer + 1) % gsize) {
+        UCPCHECK_GOTO(ucc_tl_ucp_put_nb((void *)(src + peer * nelems),
+                                        (void *)dest, nelems, peer, team, task),
+                      task, out);
+        UCPCHECK_GOTO(ucc_tl_ucp_atomic_inc(pSync, peer, team), task,
+                      out);
     }
-    task->super.super.status = status;
-    ucc_task_complete(ctask);
 
-    return UCC_OK;
+    return ucc_progress_queue_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
+out:
+    return task->super.status;
 }
 
 ucc_status_t ucc_tl_ucp_alltoall_onesided_barrier_start(ucc_coll_task_t *ctask)
@@ -72,41 +89,6 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_get_start(ucc_coll_task_t *ctask)
 }
 
 ucc_status_t ucc_tl_ucp_alltoall_onesided_progress(ucc_coll_task_t *ctask)
-{
-    ucc_tl_ucp_task_t *task   = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
-    ucc_tl_ucp_team_t *team   = TASK_TEAM(task);
-    ptrdiff_t          src    = (ptrdiff_t)TASK_ARGS(task).src.info.buffer;
-    ptrdiff_t          dest   = (ptrdiff_t)TASK_ARGS(task).dst.info.buffer;
-    size_t             nelems = TASK_ARGS(task).src.info.count;
-    ucc_rank_t         grank  = UCC_TL_TEAM_RANK(team);
-    ucc_rank_t         gsize  = UCC_TL_TEAM_SIZE(team);
-    ucc_rank_t         start  = (grank + 1) % gsize;
-    long *             pSync  = TASK_ARGS(task).global_work_buffer;
-    ucc_rank_t         peer;
-
-    ucc_tl_ucp_task_reset(task, UCC_INPROGRESS);
-    /* TODO: change when support for library-based work buffers is complete */
-    nelems = (nelems / gsize) * ucc_dt_size(TASK_ARGS(task).src.info.datatype);
-    dest   = dest + grank * nelems;
-    UCPCHECK_GOTO(ucc_tl_ucp_put_nb((void *)(src + start * nelems),
-                                    (void *)dest, nelems, start, team, task),
-                  task, out);
-    UCPCHECK_GOTO(ucc_tl_ucp_atomic_inc(pSync, start, team), task, out);
-
-    for (peer = (start + 1) % gsize; peer != start; peer = (peer + 1) % gsize) {
-        UCPCHECK_GOTO(ucc_tl_ucp_put_nb((void *)(src + peer * nelems),
-                                        (void *)dest, nelems, peer, team, task),
-                      task, out);
-        UCPCHECK_GOTO(ucc_tl_ucp_atomic_inc(pSync, peer, team), task,
-                      out);
-    }
-
-    return ucc_progress_queue_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
-out:
-    return task->super.status;
-}
-
-void ucc_tl_ucp_alltoall_onesided_progress(ucc_coll_task_t *ctask)
 {
     ucc_tl_ucp_task_t *task  = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
     ucc_tl_ucp_team_t *team  = TASK_TEAM(task);
