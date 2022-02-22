c8a6846a30e2c9e2c7879e8e088bce0170015632
ferrol aderholdt <faderholdt@nvidia.com>
2022-02-22 15:10:38 -0800

    update rate limiting

--- src/components/tl/ucp/alltoall/alltoall_onesided.c
+++ src/components/tl/ucp/alltoall/alltoall_onesided.c
@@ -27,6 +27,7 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_start(ucc_coll_task_t *ctask)
     ucc_rank_t         grank  = UCC_TL_TEAM_RANK(team);
     ucc_rank_t         gsize  = UCC_TL_TEAM_SIZE(team);
     ucc_rank_t         start  = (grank + 1) % gsize;
+    ucc_rank_t         peer;
     long *             pSync  = TASK_ARGS(task).global_work_buffer;
     ucc_rank_t         peer;
 
@@ -34,20 +35,18 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_start(ucc_coll_task_t *ctask)
     /* TODO: change when support for library-based work buffers is complete */
     nelems = (nelems / gsize) * ucc_dt_size(TASK_ARGS(task).src.info.datatype);
     dest   = dest + grank * nelems;
-        UCPCHECK_GOTO(ucc_tl_ucp_put_nb((void *)(src + grank * nelems),
-                                        (void *)dest, nelems, grank, team, task),
-                      task, out);
+    UCPCHECK_GOTO(ucc_tl_ucp_put_nb((void *)(src + start * nelems),
+                                    (void *)dest, nelems, start, team, task),
+                  task, out);
+    UCPCHECK_GOTO(ucc_tl_ucp_atomic_inc(pSync, start, team), task, out);
 
-    for (int i = 0; i < gsize; i++) {
-        int peer = (start + i) % gsize;
-        if (peer != grank) {
-            (ucc_tl_ucp_put_nb((void *)(src + peer * nelems),
-                                        (void *)dest, nelems, peer, team, task));//,
-        //              task, out);
-            (ucc_tl_ucp_atomic_inc(pSync, peer, team));//, task,
-                      //out);
-        }
-    }
+    for (peer = (start + 1) % gsize; peer != start; peer = (peer + 1) % gsize) {
+        UCPCHECK_GOTO(ucc_tl_ucp_put_nb((void *)(src + peer * nelems),
+                                        (void *)dest, nelems, peer, team, task),
+                      task, out);
+        UCPCHECK_GOTO(ucc_tl_ucp_atomic_inc(pSync, peer, team), task,
+                      out);
+    }  
 
     return ucc_progress_queue_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
 out:
@@ -155,38 +154,32 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_limit_start(ucc_coll_task_t *ctask)
     /* TODO: change when support for library-based work buffers is complete */
     nelems = (nelems / gsize) * ucc_dt_size(TASK_ARGS(task).src.info.datatype);
     dest   = dest + grank * nelems;
-    if (task->send_posted < gsize) {
-        for (;task->send_posted < gsize;) {
-            int peer = (start + task->send_posted) % gsize;
-            if ((task->send_posted - task->send_completed) < nreqs) {
-                (ucc_tl_ucp_put_nb((void *)(src + peer * nelems),
-                                        (void *)dest, nelems, peer, team, task));//,
-                polls = 0;
-            } else {
-                ucp_worker_progress(UCC_TL_UCP_TEAM_CTX(team)->ucp_worker);
-                polls++;
-            }
-            if (polls >= task->n_polls) {
-                return UCC_INPROGRESS;
-            }
+    for (;task->send_posted < gsize;) {
+        int peer = (start + task->send_posted) % gsize;
+        if ((task->send_posted - task->send_completed) < nreqs) {
+            (ucc_tl_ucp_put_nb((void *)(src + peer * nelems),
+                                    (void *)dest, nelems, peer, team, task));//,
+            (ucc_tl_ucp_atomic_inc(pSync, peer, team));
+        } else {
+            ucp_worker_progress(UCC_TL_UCP_TEAM_CTX(team)->ucp_worker);
+            polls++;
         }
-        //              task, out);
-                      //out);
+        if (polls >= task->n_polls) {
+            break;
+    //        return UCC_INPROGRESS;
+        }
     }
-
+/*
     for (int i = 0; i < gsize; i++) {
-        if (i != grank) {
-            (ucc_tl_ucp_atomic_inc(pSync, i, team));//, task,
-        }
-    } 
-
+            (ucc_tl_ucp_atomic_inc(pSync, i, team));
+    }
+*/ 
     task->super.super.status = UCC_INPROGRESS;
     status = ucc_tl_ucp_alltoall_onesided_limit_progress(&task->super);
     if (UCC_INPROGRESS == status) {
         ucc_progress_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
         return UCC_OK;
     }
-    task->super.super.status = status;
     ucc_task_complete(ctask);
     return task->super.super.status;
 }
@@ -195,10 +188,10 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_progress(ucc_coll_task_t *ctask)
 {
     ucc_tl_ucp_task_t *task  = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
     ucc_tl_ucp_team_t *team  = TASK_TEAM(task);
-    //ucc_rank_t         gsize = UCC_TL_TEAM_SIZE(team);
+    ucc_rank_t         gsize = UCC_TL_TEAM_SIZE(team);
     long *             pSync = TASK_ARGS(task).global_work_buffer;
 
-    if (/**pSync < gsize - 1 ||*/ task->send_completed < task->send_posted) {
+    if (*pSync < gsize || task->send_completed < task->send_posted) {
         ucp_worker_progress(UCC_TL_UCP_TEAM_CTX(team)->ucp_worker);
         return UCC_INPROGRESS;
     }
@@ -213,11 +206,41 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_limit_progress(ucc_coll_task_t *ctask)
 {
     ucc_tl_ucp_task_t *task  = ucc_derived_of(ctask, ucc_tl_ucp_task_t);
     ucc_tl_ucp_team_t *team  = TASK_TEAM(task);
-    ucc_rank_t         gsize = UCC_TL_TEAM_SIZE(team);
-    long *             pSync = TASK_ARGS(task).global_work_buffer;
+    ptrdiff_t          src    = (ptrdiff_t)TASK_ARGS(task).src.info.buffer;
+    ptrdiff_t          dest   = (ptrdiff_t)TASK_ARGS(task).dst.info.buffer;
+    size_t             nelems = TASK_ARGS(task).src.info.count;
+    ucc_rank_t         grank  = UCC_TL_TEAM_RANK(team);
+    ucc_rank_t         gsize  = UCC_TL_TEAM_SIZE(team);
+    ucc_rank_t         start  = (grank + 1) % gsize;
+    long *             pSync  = TASK_ARGS(task).global_work_buffer;
+    int posts = UCC_TL_UCP_TEAM_LIB(team)->cfg.alltoall_pairwise_num_posts;
+    int nreqs = (posts > gsize || posts == 0) ? gsize : posts;
+    int                polls = 0;
 
-    if ((*pSync < gsize) ||
-        (task->onesided.put_completed < task->onesided.put_posted)) {
+    /* TODO: change when support for library-based work buffers is complete */
+    nelems = (nelems / gsize) * ucc_dt_size(TASK_ARGS(task).src.info.datatype);
+    dest   = dest + grank * nelems;
+    for (;task->send_posted < gsize;) {
+        int peer = (start + task->send_posted) % gsize;
+        if ((task->send_posted - task->send_completed) < nreqs) {
+            (ucc_tl_ucp_put_nb((void *)(src + peer * nelems),
+                                    (void *)dest, nelems, peer, team, task));//,
+            (ucc_tl_ucp_atomic_inc(pSync, peer, team));
+        } else {
+            ucp_worker_progress(UCC_TL_UCP_TEAM_CTX(team)->ucp_worker);
+            polls++;
+        }
+        if (polls >= task->n_polls) {
+            return UCC_INPROGRESS;
+        }
+    }
+//    ucc_tl_ucp_flush(team);
+/*
+    for (int i = 0; i < gsize; i++) {
+            (ucc_tl_ucp_atomic_inc(pSync, i, team));
+    }
+*/ 
+    if (*pSync < gsize || task->onesided.put_completed < task->onesided.put_posted) {
         ucp_worker_progress(UCC_TL_UCP_TEAM_CTX(team)->ucp_worker);
         return;
     }
