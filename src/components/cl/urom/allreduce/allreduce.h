9345b6ec9bb360bc9a6054e4d952887f000bbbb9
ferrol aderholdt <faderholdt@nvidia.com>
2024-08-09 11:11:38 -0700

    snapshot allreduce support

--- src/components/cl/urom/allreduce/allreduce.h
+++ src/components/cl/urom/allreduce/allreduce.h
@@ -6,35 +6,28 @@
 
 #ifndef ALLREDUCE_H_
 #define ALLREDUCE_H_
-#include "../cl_hier.h"
+#include "../cl_urom_coll.h"
 
-enum
-{
-    UCC_CL_HIER_ALLREDUCE_ALG_RAB,
-    UCC_CL_HIER_ALLREDUCE_ALG_SPLIT_RAIL,
-    UCC_CL_HIER_ALLREDUCE_ALG_LAST,
+enum {
+    UCC_CL_UROM_ALLREDUCE_ALG_FULL,
+    UCC_CL_UROM_ALLREDUCE_ALG_LAST,
 };
 
 extern ucc_base_coll_alg_info_t
-    ucc_cl_hier_allreduce_algs[UCC_CL_HIER_ALLREDUCE_ALG_LAST + 1];
+    ucc_cl_urom_allreduce_algs[UCC_CL_HIER_ALLREDUCE_ALG_LAST + 1];
 
-#define UCC_CL_HIER_ALLREDUCE_DEFAULT_ALG_SELECT_STR "allreduce:0-4k:@rab"
+#define UCC_CL_UROM_ALLREDUCE_DEFAULT_ALG_SELECT_STR "allreduce:0-inf:@full"
 
-ucc_status_t ucc_cl_hier_allreduce_rab_init(ucc_base_coll_args_t *coll_args,
-                                            ucc_base_team_t      *team,
-                                            ucc_coll_task_t     **task);
+ucc_status_t ucc_cl_urom_allreduce_full_init(ucc_base_coll_args_t *coll_args,
+                                             ucc_base_team_t      *team,
+                                             ucc_coll_task_t     **task);
 
-ucc_status_t
-ucc_cl_hier_allreduce_split_rail_init(ucc_base_coll_args_t *coll_args,
-                                      ucc_base_team_t      *team,
-                                      ucc_coll_task_t     **task);
-
-static inline int ucc_cl_hier_allreduce_alg_from_str(const char *str)
+static inline int ucc_cl_urom_allreduce_alg_from_str(const char *str)
 {
     int i;
 
-    for (i = 0; i < UCC_CL_HIER_ALLREDUCE_ALG_LAST; i++) {
-        if (0 == strcasecmp(str, ucc_cl_hier_allreduce_algs[i].name)) {
+    for (i = 0; i < UCC_CL_UROM_ALLREDUCE_ALG_LAST; i++) {
+        if (0 == strcasecmp(str, ucc_cl_urom_allreduce_algs[i].name)) {
             break;
         }
     }
