913ff14a63da99ab42af80cf3ad58601bf5bd230
ferrol aderholdt <faderholdt@nvidia.com>
2024-08-28 11:38:36 -0700

    TL/UCP: add openshmem optimal sliding window allreduce alg

--- src/components/tl/ucp/tl_ucp_coll.c
+++ src/components/tl/ucp/tl_ucp_coll.c
@@ -262,6 +262,9 @@ ucc_status_t ucc_tl_ucp_alg_id_to_init(int alg_id, const char *alg_id_str,
         case UCC_TL_UCP_ALLREDUCE_ALG_RING:
             *init = ucc_tl_ucp_allreduce_ring_init;
             break;
+        case UCC_TL_UCP_ALLREDUCE_ALG_SLIDING_WINDOW_OSHMEM:
+            *init = ucc_tl_ucp_allreduce_sliding_window_oshmem_init;
+            break;
         default:
             status = UCC_ERR_INVALID_PARAM;
             break;
