59822c0c78f5cbfb9a48bd837b6e239563c0f908
Ferrol Aderholdt <ferrol.aderholdt@gmail.com>
2026-01-26 16:25:08 -0800

    REVIEW: fix errors

--- src/components/tl/ucp/allreduce/allreduce_ring.c
+++ src/components/tl/ucp/allreduce/allreduce_ring.c
@@ -26,7 +26,36 @@
 static ucc_status_t
 ucc_tl_ucp_allreduce_ring_start(ucc_coll_task_t *task)
 {
+    ucc_schedule_t  *schedule = ucc_derived_of(task, ucc_schedule_t);
+    ucc_coll_args_t *args     = &schedule->super.bargs.args;
+    size_t           count;
+    size_t           data_size;
+    ucc_status_t     status;
+
     UCC_TL_UCP_PROFILE_REQUEST_EVENT(task, "ucp_allreduce_ring_start", 0);
+
+    /* Handle in-place vs out-of-place cases */
+    if (UCC_IS_INPLACE(*args)) {
+        /* Case 1: Allreduce is in-place (src == dst)
+         * Data is already in dst buffer, no copy needed.
+         * Reduce-scatter and allgather will both operate in-place on dst. */
+    } else {
+        /* Case 2: Allreduce is NOT in-place (src != dst)
+         * Copy src to dst before starting reduce-scatter.
+         * This is necessary because both reduce_scatter and allgather are
+         * configured to operate in-place on dst buffer to ensure proper
+         * offset placement (each rank's data at rank * count/tsize). */
+        count     = args->dst.info.count;
+        data_size = count * ucc_dt_size(args->dst.info.datatype);
+        status    = ucc_mc_memcpy(args->dst.info.buffer,
+                                  args->src.info.buffer,
+                                  data_size,
+                                  args->dst.info.mem_type,
+                                  args->src.info.mem_type);
+        if (ucc_unlikely(status != UCC_OK)) {
+            return status;
+        }
+    }
     return ucc_schedule_start(task);
 }
 
@@ -46,9 +75,9 @@ ucc_status_t ucc_tl_ucp_allreduce_ring_init(ucc_base_coll_args_t *coll_args,
                                             ucc_base_team_t *team,
                                             ucc_coll_task_t **task_h)
 {
-    ucc_tl_ucp_team_t     *tl_team   = ucc_derived_of(team, ucc_tl_ucp_team_t);
-    ucc_rank_t             tsize     = UCC_TL_TEAM_SIZE(tl_team);
-    size_t                 count     = coll_args->args.dst.info.count;
+    ucc_tl_ucp_team_t     *tl_team = ucc_derived_of(team, ucc_tl_ucp_team_t);
+    ucc_rank_t             tsize   = UCC_TL_TEAM_SIZE(tl_team);
+    size_t                 count   = coll_args->args.dst.info.count;
     ucc_schedule_t        *schedule;
     ucc_coll_task_t       *rs_task;
     ucc_coll_task_t       *ag_task;
@@ -60,6 +89,11 @@ ucc_status_t ucc_tl_ucp_allreduce_ring_init(ucc_base_coll_args_t *coll_args,
     if (!ucc_coll_args_is_predefined_dt(&coll_args->args, UCC_RANK_INVALID)) {
         tl_error(team->context->lib,
                  "user defined datatype is not supported");
+        return UCC_ERR_NOT_SUPPORTED;
+    }
+
+    if (UCC_TL_UCP_TEAM_LIB(tl_team)->cfg.reduce_avg_pre_op &&
+        coll_args->args.op == UCC_OP_AVG) {
         return UCC_ERR_NOT_SUPPORTED;
     }
 
@@ -91,9 +125,14 @@ ucc_status_t ucc_tl_ucp_allreduce_ring_init(ucc_base_coll_args_t *coll_args,
      * - dst.info.count should be the per-rank output count (count/tsize)
      * - The algorithm internally computes total = dst.info.count * tsize
      */
-    rs_args                     = *coll_args;
-    rs_args.args.coll_type      = UCC_COLL_TYPE_REDUCE_SCATTER;
-    rs_args.args.dst.info.count = count / tsize;
+    rs_args                        = *coll_args;
+    rs_args.args.coll_type         = UCC_COLL_TYPE_REDUCE_SCATTER;
+    rs_args.args.src.info.buffer   = coll_args->args.dst.info.buffer;
+    rs_args.args.src.info.mem_type = coll_args->args.dst.info.mem_type;
+    rs_args.args.dst.info.count    = count;
+    rs_args.args.mask             |= UCC_COLL_ARGS_FIELD_FLAGS;
+    rs_args.args.flags            |= UCC_COLL_ARGS_FLAG_IN_PLACE;
+
 
     status = ucc_tl_ucp_reduce_scatter_ring_init(&rs_args, team, &rs_task);
     if (ucc_unlikely(UCC_OK != status)) {
@@ -118,11 +157,14 @@ ucc_status_t ucc_tl_ucp_allreduce_ring_init(ucc_base_coll_args_t *coll_args,
      * - dst.info.count should be the total output count
      * - IN_PLACE flag since data is already in correct position in dst buffer
      */
-    ag_args                     = *coll_args;
-    ag_args.args.coll_type      = UCC_COLL_TYPE_ALLGATHER;
-    ag_args.args.mask          |= UCC_COLL_ARGS_FIELD_FLAGS;
-    ag_args.args.flags          = UCC_COLL_ARGS_FLAG_IN_PLACE;
-    ag_args.args.dst.info.count = count;
+    ag_args                      = *coll_args;
+    ag_args.args.coll_type       = UCC_COLL_TYPE_ALLGATHER;
+    ag_args.args.src.info.buffer = NULL;
+    ag_args.args.src.info.count  = 0;
+    ag_args.args.dst.info.buffer = coll_args->args.dst.info.buffer;
+    ag_args.args.dst.info.count  = count;
+    ag_args.args.mask           |= UCC_COLL_ARGS_FIELD_FLAGS;
+    ag_args.args.flags          |= UCC_COLL_ARGS_FLAG_IN_PLACE;
 
     status = ucc_tl_ucp_allgather_ring_init(&ag_args, team, &ag_task);
     if (ucc_unlikely(UCC_OK != status)) {
@@ -134,7 +176,7 @@ ucc_status_t ucc_tl_ucp_allreduce_ring_init(ucc_base_coll_args_t *coll_args,
     UCC_CHECK_GOTO(ucc_schedule_add_task(schedule, ag_task), err_ag, status);
     UCC_CHECK_GOTO(ucc_task_subscribe_dep(rs_task, ag_task,
                                           UCC_EVENT_COMPLETED),
-                   err_ag, status);
+                                          err_ag, status);
 
     /* Set up schedule callbacks */
     schedule->super.post     = ucc_tl_ucp_allreduce_ring_start;
