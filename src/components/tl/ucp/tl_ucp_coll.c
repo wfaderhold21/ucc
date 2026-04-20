c3c8435fbfc4f41d93563e7a76bc49fb9971da0d
Ferrol Aderholdt <faderholdt@nvidia.com>
2026-04-20 13:25:49 -0700

    REVIEW: address feedback

--- src/components/tl/ucp/tl_ucp_coll.c
+++ src/components/tl/ucp/tl_ucp_coll.c
@@ -361,6 +361,9 @@ ucc_status_t ucc_tl_ucp_alg_id_to_init(int alg_id, const char *alg_id_str,
         case UCC_TL_UCP_BARRIER_ALG_KNOMIAL:
             *init = ucc_tl_ucp_barrier_knomial_init;
             break;
+        case UCC_TL_UCP_BARRIER_ALG_KNOMIAL_AM:
+            *init = ucc_tl_ucp_barrier_knomial_am_init;
+            break;
         default:
             status = UCC_ERR_INVALID_PARAM;
             break;
